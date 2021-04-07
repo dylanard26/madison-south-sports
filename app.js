3b8e5b93a713aa5c9c74447694a9543fc2983c7d
Dylan Ard <77060900+dylanard26@users.noreply.github.com>
2021-04-07 08:46:40 -0500

    dababy

--- app.js
+++ app.js
@@ -12,11 +12,8 @@ const navSlide = () => {
         if(link.style.animation){
             link.style.animation = '';
         } else {
-<<<<<<< HEAD
-            link.style.animation=`navLinkFade 1.0s ease forwards ${index / 7+0.2}s`;
-=======
+            
             link.style.animation=`navLinkFade 0.5s ease forwards ${index / 7+0.5}s`;
->>>>>>> 905cd219421d2525a67410beb3986381b064216b
         }
     });
     //burger animation
